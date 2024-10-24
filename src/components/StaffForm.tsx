8ddf8543b3ea9da64ee3cddce5d937f46ef5527a
Jean Baradat <monsieur.jean.baradat@gmail.com>
2024-10-25 00:53:32 +0200

    test with dialog

--- src/components/StaffForm.tsx
+++ src/components/StaffForm.tsx
@@ -4,7 +4,7 @@ import { z } from "zod"
 import { Button } from "@/components/shadcn/button"
 import { getMonthsList, getYearsList } from "@/utils/dateUtils"
 import { Form } from "@/components/shadcn/form"
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import FormFieldDefaultInput from "@/components/FormFieldDefaultInput"
 import FormFieldDateInput from "@/components/FormFieldDateInput"
 import AutoComplete from "@/components/AutoComplete"
@@ -20,6 +20,13 @@ import {
 import FormFieldSelect from "@/components/FormFieldSelect"
 import { states } from "@/db/states"
 import { departments } from "@/db/departments"
+import {
+	Dialog,
+	DialogTrigger,
+	DialogContent,
+	handleDialog,
+} from "@jean/rc-dialog"
+import "/node_modules/@jean/rc-dialog/dist/style.css"
 
 /**
  * Validation schema for the staff form.
@@ -140,6 +147,8 @@ const StaffForm = () => {
 	const staffFormFieldsData = useSelector(staffFormFieldsState)
 	const addressFound = useSelector(staffFormAddressFoundState)
 
+	const [isOpen, setIsOpen] = useState(false)
+
 	const defaultValues: StaffFormFields = {
 		"first-name": "",
 		"last-name": "",
@@ -183,6 +192,8 @@ const StaffForm = () => {
 		dispatch(submitForm({ ...data, key: generateKey(data) }))
 		dispatch(resetAddressFound())
 		reset(defaultValues)
+
+		handleDialog(isOpen, setIsOpen)
 	}
 
 	/**
@@ -235,148 +246,182 @@ const StaffForm = () => {
 		<Form {...form}>
 			<form
 				onSubmit={handleSubmit(onSubmit)}
-				className="mx-auto max-w-3xl space-y-6 rounded border bg-background p-5 shadow-[-4px_9px_25px_-6px_rgba(0,0,0,0.1)]"
+				className="mx-auto max-w-3xl rounded border bg-background p-5 shadow-[-4px_9px_25px_-6px_rgba(0,0,0,0.1)]"
 			>
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-					<FormFieldDefaultInput
-						form={{ control, errors }}
-						name="first-name"
-						label="First Name"
-						input={{ maxLength: 30, placeholder: "John" }}
-						invalidStringMessage={{
-							content: `
+				<div className="space-y-6 pb-6">
+					<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+						<FormFieldDefaultInput
+							form={{ control, errors }}
+							name="first-name"
+							label="First Name"
+							input={{ maxLength: 30, placeholder: "John" }}
+							invalidStringMessage={{
+								content: `
 								Please enter only letters, with or without accents.
 								You can include spaces, hyphens or an apostrophe between words. 
 								Your entry must not exceed three words. Trim any leading 
 								or trailing spaces from your input.
 							`,
-							example:
-								"Examples: Jean-Pierre, O'Connor, محمد, Иван, María José D.",
-						}}
-					/>
+								example:
+									"Examples: Jean-Pierre, O'Connor, محمد, Иван, María José D.",
+							}}
+						/>
 
-					<FormFieldDefaultInput
-						form={{ control, errors }}
-						name="last-name"
-						label="Last Name"
-						input={{ maxLength: 30, placeholder: "Doe" }}
-						invalidStringMessage={{
-							content: `
+						<FormFieldDefaultInput
+							form={{ control, errors }}
+							name="last-name"
+							label="Last Name"
+							input={{ maxLength: 30, placeholder: "Doe" }}
+							invalidStringMessage={{
+								content: `
 								Please enter only letters, with or without accents.
 								You can include spaces, hyphens or an apostrophe
 								between words. Your entry must not exceed three words.
 								Trim any leading or trailing spaces from your input.
 							`,
-							example: "Examples: Dubois, Fitzgerald, الحسن, Петров, Rodríguez",
-						}}
-					/>
-				</div>
+								example:
+									"Examples: Dubois, Fitzgerald, الحسن, Петров, Rodríguez",
+							}}
+						/>
+					</div>
 
-				<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-					<FormFieldDateInput
-						form={{ control, errors }}
-						name="date-of-birth"
-						label="Date of Birth"
-						years={years}
-						months={months}
-						input={{ maxLength: 10, placeholder: "02-22-1994" }}
-					/>
-
-					<FormFieldDateInput
-						form={{ control, errors }}
-						name="date-of-start"
-						label="Date of Start"
-						years={years}
-						months={months}
-						input={{ maxLength: 10, placeholder: "02-22-1994" }}
-					/>
-				</div>
-
-				<fieldset className="min-w-0 rounded-md border p-4">
-					<legend className="px-2 text-sm font-semibold">Address</legend>
-					<div className="flex flex-col gap-4">
-						<AutoComplete
-							inputPlaceholder="e.g. 1600 Pennsylvania Avenue NW, Washington, DC 20500"
-							displayName={addressFound.display_name}
+					<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+						<FormFieldDateInput
+							form={{ control, errors }}
+							name="date-of-birth"
+							label="Date of Birth"
+							years={years}
+							months={months}
+							input={{ maxLength: 10, placeholder: "02-22-1994" }}
 						/>
 
-						<div className="grid gap-4 rounded-lg border bg-muted/10 px-3 py-2">
-							<FormFieldDefaultInput
-								form={{ control, errors }}
-								name="street"
-								label="Street"
-								input={{ maxLength: 100, placeholder: "1234 Main St" }}
-								invalidStringMessage={{
-									content: `
+						<FormFieldDateInput
+							form={{ control, errors }}
+							name="date-of-start"
+							label="Date of Start"
+							years={years}
+							months={months}
+							input={{ maxLength: 10, placeholder: "02-22-1994" }}
+						/>
+					</div>
+
+					<fieldset className="min-w-0 rounded-md border p-4">
+						<legend className="px-2 text-sm font-semibold">Address</legend>
+						<div className="flex flex-col gap-4">
+							<AutoComplete
+								inputPlaceholder="e.g. 1600 Pennsylvania Avenue NW, Washington, DC 20500"
+								displayName={addressFound.display_name}
+							/>
+
+							<div className="grid gap-4 rounded-lg border bg-muted/10 px-3 py-2">
+								<FormFieldDefaultInput
+									form={{ control, errors }}
+									name="street"
+									label="Street"
+									input={{ maxLength: 100, placeholder: "1234 Main St" }}
+									invalidStringMessage={{
+										content: `
 										Please enter a valid address, starting with its number, 
 										then its name and type, and other information if necessary. 
 										You can include spaces, hyphens, commas, dots or a pound sign.
 									`,
-									example:
-										"Exemples : 123 Main St, 42 Broadway Ave #1234, 1600 Pennsylvania Avenue NW",
-								}}
-								autocompleteValue={handleAutocompleteValueStreet()}
-							/>
-							<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-								<FormFieldDefaultInput
-									form={{ control, errors }}
-									name="city"
-									label="City"
-									input={{ maxLength: 100, placeholder: "New York" }}
-									invalidStringMessage={{
-										content: `
+										example:
+											"Exemples : 123 Main St, 42 Broadway Ave #1234, 1600 Pennsylvania Avenue NW",
+									}}
+									autocompleteValue={handleAutocompleteValueStreet()}
+								/>
+								<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+									<FormFieldDefaultInput
+										form={{ control, errors }}
+										name="city"
+										label="City"
+										input={{ maxLength: 100, placeholder: "New York" }}
+										invalidStringMessage={{
+											content: `
 											Please enter a valid city name. It should contain only letters, 
 											spaces, hyphens, or apostrophes.
 										`,
-										example:
-											"Examples: New York, San Francisco, Winston-Salem, O'Fallon",
-									}}
-									autocompleteValue={handleAutocompleteValueCity()}
-								/>
-								<FormFieldSelect
-									form={{ control, errors }}
-									name="state"
-									label="State"
-									dataList={states}
-									placeholder="Select a state"
-									tooltipContent="Delete the state"
-									autocompleteValue={addressFound.address.state}
-								/>
-							</div>
+											example:
+												"Examples: New York, San Francisco, Winston-Salem, O'Fallon",
+										}}
+										autocompleteValue={handleAutocompleteValueCity()}
+									/>
+									<FormFieldSelect
+										form={{ control, errors }}
+										name="state"
+										label="State"
+										dataList={states}
+										placeholder="Select a state"
+										tooltipContent="Delete the state"
+										autocompleteValue={addressFound.address.state}
+									/>
+								</div>
 
-							<FormFieldDefaultInput
-								form={{ control, errors }}
-								name="zipCode"
-								label="ZIP Code"
-								input={{ maxLength: 10, placeholder: "10001" }}
-								invalidStringMessage={{
-									content: `
+								<FormFieldDefaultInput
+									form={{ control, errors }}
+									name="zipCode"
+									label="ZIP Code"
+									input={{ maxLength: 10, placeholder: "10001" }}
+									invalidStringMessage={{
+										content: `
 										Please enter a valid ZIP code. It should be a 5-digit number 
 										or a 9-digit number with a hyphen after the first 5 digits.
 									`,
-									example: "Examples: 12345, 12345-6789",
-								}}
-								autocompleteValue={addressFound.address.postcode}
-							/>
+										example: "Examples: 12345, 12345-6789",
+									}}
+									autocompleteValue={addressFound.address.postcode}
+								/>
+							</div>
 						</div>
-					</div>
-				</fieldset>
+					</fieldset>
 
-				<FormFieldSelect
-					form={{ control, errors }}
-					name="department"
-					label="Department"
-					dataList={departments}
-					placeholder="Select a department"
-					tooltipContent="Delete the department"
-				/>
+					<FormFieldSelect
+						form={{ control, errors }}
+						name="department"
+						label="Department"
+						dataList={departments}
+						placeholder="Select a department"
+						tooltipContent="Delete the department"
+					/>
+				</div>
 
-				<Button
-					type="submit"
-					className="w-full"
+				<Dialog
+					isOpen={isOpen}
+					setIsOpen={setIsOpen}
 				>
-					Save
-				</Button>
+					<DialogTrigger onClick>
+						<Button
+							type="submit"
+							className="w-full"
+						>
+							Save
+						</Button>
+					</DialogTrigger>
+					<DialogContent
+						titleId="dialogTitle"
+						descriptionId="dialogDescription"
+					>
+						<h1
+							id="dialogTitle"
+							className="pb-3 text-2xl font-semibold"
+						>
+							Lorem, ipsum dolor.
+						</h1>
+						<p
+							id="dialogDescription"
+							className="pb-3"
+						>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+							Accusamus error voluptatibus eligendi expedita cum dignissimos
+							repellat dolorum pariatur laboriosam, quos doloribus, ab est
+							placeat nobis ex incidunt adipisci dolore doloremque.
+						</p>
+						<div className="flex gap-4">
+							<Button className="flex-1">Lorem, ipsum.</Button>
+							<Button className="flex-1">Lorem, ipsum dolor.</Button>
+						</div>
+					</DialogContent>
+				</Dialog>
 			</form>
 		</Form>
 	)
