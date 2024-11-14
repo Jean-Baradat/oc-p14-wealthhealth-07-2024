52007441d927771d46e5bb94e9601be4f8f3e993
Jean Baradat <monsieur.jean.baradat@gmail.com>
2024-11-14 17:15:11 +0100

    fix vercek-build.js script

--- scripts/vercel-build.js
+++ scripts/vercel-build.js
@@ -1,10 +1,11 @@
-import { execSync } from "child_process"
-import process from "process"
+/* eslint-disable no-undef */
 
 if (process.env.VERCEL_ENV === "production") {
 	try {
+		console.log("Running production build...")
 		execSync("npm run build:prod", { stdio: "inherit" })
 	} catch (error) {
+		console.log("Running preview build...")
 		process.exit(1)
 	}
 }
